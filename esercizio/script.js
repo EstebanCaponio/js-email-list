4c0bcb852829303e20375b92d6f45303a7f5d22b
EstebanCaponio <estebancaponio@gmail.com>
2025-03-13 16:02:13 +0100

    feat: added refresh

--- esercizio/script.js
+++ esercizio/script.js
@@ -1,21 +1,47 @@
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
 const emailList = document.getElementById('email-list');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let emails = '';
+
+    for (let i = 1; i <= 10; i++) {
+        console.log('ciao');
+        axios.get(endpoint)
+            .then((response) => {
+                let email = response.data.response;
+                console.log(email);
+
+                emails += `<li class="list-group-item">${email}</li>`;
+
+                emailList.innerHTML = emails;
+
+            })
+            .catch(error => {
+                console.log('error:', error);
+            });
+    }
+
+})
+
 let emails = '';
 
-for (i = 1; i <= 10; i++) {
+    for (let i = 1; i <= 10; i++) {
+        console.log('ciao');
+        axios.get(endpoint)
+            .then((response) => {
+                let email = response.data.response;
+                console.log(email);
 
-    axios.get(endpoint)
-        .then((response) => {
-            let email = response.data.response;
-            console.log(email);
+                emails += `<li class="list-group-item">${email}</li>`;
 
-            emails += `<li class="list-group-item">${email}</li>`;
+                emailList.innerHTML = emails;
 
-            emailList.innerHTML = emails;
-
-        })
-        .catch(error => {
-            console.log('error:', error);
-        });
-}
+            })
+            .catch(error => {
+                console.log('error:', error);
+            });
+    }
