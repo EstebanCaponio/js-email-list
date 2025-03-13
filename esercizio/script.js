3c0d81cb7fe21cd95379d8d27d2c90dd5d137433
EstebanCaponio <estebancaponio@gmail.com>
2025-03-13 15:23:11 +0100

    feat: generated 10emails random

--- esercizio/script.js
+++ esercizio/script.js
@@ -1,10 +1,21 @@
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
-axios.get(endpoint)
-    .then((response) => {
-        console.log(response.data.response);
-    })
-    .catch(error =>{
-        console.log('error:',error);
-    });
-    
+const emailList = document.getElementById('email-list');
+let emails = '';
+
+for (i = 0; i <= 10; i++) {
+
+    axios.get(endpoint)
+        .then((response) => {
+            let email = response.data.response;
+            console.log(email);
+
+            emails += `<li class="list-group-item">${email}</li>`;
+
+            emailList.innerHTML = emails;
+
+        })
+        .catch(error => {
+            console.log('error:', error);
+        });
+}
