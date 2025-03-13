1ddae98ca529c46550459d257a7ef8a68e068f0a
EstebanCaponio <estebancaponio@gmail.com>
2025-03-13 14:56:48 +0100

    feat: scaffoldig

---
+++ esercizio/script.js
@@ -0,0 +1,10 @@
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+axios.get(endpoint)
+    .then((response) => {
+        console.log(response.data.response);
+    })
+    .catch(error =>{
+        console.log('error:',error);
+    });
+    
