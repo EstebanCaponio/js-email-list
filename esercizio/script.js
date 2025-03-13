8872ddd07369fa01929e1170adef5e091e252284
EstebanCaponio <estebancaponio@gmail.com>
2025-03-13 16:14:33 +0100

    fix: created function

--- esercizio/script.js
+++ esercizio/script.js
@@ -3,13 +3,23 @@ const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 const emailList = document.getElementById('email-list');
 const form = document.querySelector('form');
 
+
+
+tenMails();
+
 form.addEventListener('submit', function (event) {
     event.preventDefault();
+
+    tenMails()
+})
+
+
+// funzione
+function tenMails(){
 
     let emails = '';
 
     for (let i = 1; i <= 10; i++) {
-        console.log('ciao');
         axios.get(endpoint)
             .then((response) => {
                 let email = response.data.response;
@@ -25,23 +35,4 @@ form.addEventListener('submit', function (event) {
             });
     }
 
-})
-
-let emails = '';
-
-    for (let i = 1; i <= 10; i++) {
-        console.log('ciao');
-        axios.get(endpoint)
-            .then((response) => {
-                let email = response.data.response;
-                console.log(email);
-
-                emails += `<li class="list-group-item">${email}</li>`;
-
-                emailList.innerHTML = emails;
-
-            })
-            .catch(error => {
-                console.log('error:', error);
-            });
-    }
+}
